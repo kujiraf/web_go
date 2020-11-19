f9ee108ca7f3d48ccdc1bdeeee01e5738b2a822b
root <frkw123@gmail.com>
2020-11-20 08:50:44 +0900

    add atomic

---
+++ fundamental/Readme.md
@@ -0,0 +1,3 @@
+# Fundamental of golang
+
+* [このUdemyコース](https://nttdatagdpo.udemy.com/course/learn-how-to-code/learn/lecture/3993384#overview)で学んだコード
--- fundamental/concurrency/go_routine.go
+++ fundamental/concurrency/go_routine.go
@@ -4,11 +4,41 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
 	// Race()
-	Mutex()
+	// Mutex()
+	Atomic()
+}
+
+/*
+Atomic is sample func of go routine which data is consistent.
+
+go run go_routine.goでも実行結果が100で安定する
+*/
+func Atomic() {
+	fmt.Println("CPUs:", runtime.NumCPU())             // 1つのCPUで稼働している
+	fmt.Println("Goroutines:", runtime.NumGoroutine()) // 1つのgo routineで稼働している
+
+	var counter int64
+
+	gs := 100
+	var wg sync.WaitGroup
+	wg.Add(gs) // いくつのgo routineを待って関数を完了するかを設定する
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			atomic.AddInt64(&counter, 1)
+			runtime.Gosched()
+			fmt.Println("Counter\t", atomic.LoadInt64(&counter))
+			wg.Done() // go routineが完了したことを通知する
+		}()
+		// fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
 }
 
 /*
@@ -31,10 +61,11 @@ func Mutex() {
 	for i := 0; i < gs; i++ {
 		go func() {
 			mu.Lock() // ロックする
-			v := counter
+			// v := counter
 			runtime.Gosched() // 他のgo routineの呼び出しを許可する。time.Sleepでも次の呼び出しをすることができるが、こっちの方が効率的
-			v++
-			counter = v
+			// v++
+			// counter = v
+			counter++
 			mu.Unlock() // ロック解除
 			wg.Done()   // go routineが完了したことを通知する
 		}()
